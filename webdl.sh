552f8c4a66864b6fcfda71875f1d9c28c4ec8fb5
Apitpr0 <apitpro123@gmail.com>
2022-07-05 12:28:31 +0800

    follow recommendation from webmaster

--- webdl.sh
+++ webdl.sh
@@ -15,26 +15,17 @@ echo "Masukkan nama video yang ingin digunakan:"
 read -r videoOut
 echo "Masukkan nama audio yang ingin digunakan:"
 read -r audioOut
-#start to download stuff using ytdlp
-echo "Muat turun video dimulakan"
-yt-dlp --allow-u -f "$vidID" "$mpdlink" -o "$videoOut" -N 60
-echo "Muat turun audio dimulakan"
-yt-dlp --allow-u -f "$audID" "$mpdlink" -o "$audioOut" -N 60
+#start to download stuff using ytdlp and add new method to download both video and audio
+yt-dlp --allow-u "$vidID"+"$audID" "$MPD" -o d -N 20
 #start to decrypt video and audio using mp4decrypt
 echo "Masukkan KID:KEY yang anda dapat:"
 read -r key
-#request for decrypted filename for both aud and vid
-echo "Masukkan nama file video yang sudah di-decrypt:"
-read -r videoDec
-echo "Masukkan nama file audio yang sudah di-decrypt:"
-read -r audioDec
-#start to decrypt video and audio using mp4decrypt
 echo "Decrypt video dan audio dimulakan"
-mp4decrypt --show-progress --key "$key" "$videoOut" "$videoDec"
-mp4decrypt --show-progress --key "$key" "$audioOut" "$audioDec"
+mp4decrypt --show-progress --key "$key" d.*.mp4 dec.mp4 && mp4decrypt --show-progress --key "$key" d.*.m4a dec.m4a
 #start to merge video and audio using ffmpeg
 echo "Mulakan penggabungan video dan audio"
 echo "Masukkan nama file complete yang anda inginkan:"
 read -r finalvidName
-ffmpeg -i "$videoDec" -i "$audioDec" -c copy "$finalvidName"
+ffmpeg -v quiet -stats -i "$videoDec" -i "$audioDec" -c copy "$finalvidName" 
+mv -v "$finalvidName" /var/content/www/html/
 echo "Siap boss. Selamat merilis :)"
